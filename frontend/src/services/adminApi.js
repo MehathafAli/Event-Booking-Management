93be5b10a3ec575f12effa64baadcb1756aaf02c
MehathafAli <147412734+MehathafAli@users.noreply.github.com>
2026-09-24 18:10:36 +0530

    Refactor settings to use environment

--- frontend/src/services/adminApi.js
+++ frontend/src/services/adminApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const adminApi = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'https://event-booking-management-production.up.railway.app/api/',
   timeout: 20000,
 })
 
